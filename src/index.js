3db9fae1c5271945b0611abdc49c485851a50561
yash <yash04577@gmail.com>
2022-12-24 19:22:18 +0530

    database added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config({path: "../config.env"});
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000
+require("../DB/connection");
+
+
+//redirecting to router to check and process the request
+app.use(require("../router/routes"))
 
 app.get("/", (req, res)=>{
     res.send("welcome to backend home page");
